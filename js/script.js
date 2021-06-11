1a9b5939c5206a8056ca6308e211cf363cae7de3
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-11 15:18:38 +0200

    game

--- js/script.js
+++ js/script.js
@@ -2,3 +2,30 @@
 Da li parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+var n = 5;
+var numeri = geneRandom(1, 100, n);
+var nUtente = [];
+var time = 3;
+
+alert("Memorizza questi numeri: \r\n" + numeri);
+setTimeout(askNum, time*1000);
+
+function askNum() {
+    while (nUtente.length < n) {
+        nUtente.push(parseInt(prompt("Inserisci numero")));
+    }
+
+    document.getElementById("numeri").innerHTML = "I numeri da ricordare erano: " + numeri + "<br>Numeri ricordati correttamente:";
+
+    for (var i = 0; i < numeri.length; i++) {
+        if (numeri.includes(nUtente[i])) document.getElementById("numeri").innerHTML += " " + numeri[i];
+    }
+}
+
+function geneRandom(min, max, n) {
+    var arr = [];
+    while (arr.length < n) {
+        arr.push(Math.floor(Math.random() * (max - min) + min));
+    }
+    return arr;
+}
